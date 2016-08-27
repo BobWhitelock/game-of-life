d475d00d03dee362d032189c0e8ce87ba1929507
Bob Whitelock <bob.whitelock1@gmail.com>
2016-08-27 23:48:19 +0100

    Implement World#frontier for the general case

--- src/World.js
+++ src/World.js
@@ -1,6 +1,8 @@
 
 import _ from 'lodash'
 
+const COORD_DIFFS = [-1, 0, 1]
+
 export default class World {
   constructor(livingCells) {
     this._livingCells = livingCells
@@ -10,16 +12,19 @@ export default class World {
     return this._livingCells
   }
 
+  // An array of all cells which may potentially be changed in the next
+  // iteration (the set of all currently alive cells and their neighbours).
   frontier() {
-    const cell = this._livingCells[0]
-    const frontier = []
-
-    _.forEach([-1, 0, 1], x => {
-      _.forEach([-1, 0, 1], y => {
-        frontier.push({x: cell.x + x, y: cell.y + y})
+    return _.chain(this.livingCells())
+    .map(cell => {
+      return _.map(COORD_DIFFS, x => {
+        return _.map(COORD_DIFFS, y => {
+          return {x: cell.x + x, y: cell.y + y}
+        })
       })
     })
-
-    return frontier
+    .flattenDeep()
+    .uniqWith(_.isEqual)
+    .value()
   }
 }
--- src/__tests__/World.test.js
+++ src/__tests__/World.test.js
@@ -14,18 +14,16 @@ describe('World', function() {
   })
 
   describe('#frontier', function() {
-    context('when one living cell', function() {
-      it('returns the frontier', function() {
-        const world = new World([{x: 1, y: 2}])
+    it('returns the frontier', function() {
+      const world = new World([{x: 1, y: 2}, {x: 2, y: 2}])
 
-        const frontier = world.frontier()
-        expect(_.sortBy(frontier, ['x', 'y'])).to.deep.equal([
-            {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3},
-            {x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3},
-            {x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3},
-        ])
-      })
+      const frontier = world.frontier()
+      expect(_.sortBy(frontier, ['x', 'y'])).to.deep.equal([
+          {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3},
+          {x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3},
+          {x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3},
+          {x: 3, y: 1}, {x: 3, y: 2}, {x: 3, y: 3},
+      ])
     })
   })
-
 })
